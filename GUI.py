0d9bf7dea1e98a2964d4834588ea34dd15a3035e
Tyler Lott <Tylerlott0126@gmail.com>
2019-08-11 11:31:09 -0600

    added checkbox for showing principal plane, also added better labeling for the plot legend

--- GUI.py
+++ GUI.py
@@ -8,6 +8,8 @@ from PIL import Image, ImageTk
 
 tmpdir = tempfile.mkdtemp()
 print(tmpdir)
+
+
 class App(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
@@ -16,7 +18,7 @@ class App(tk.Frame):
 
         x = (self.master.winfo_screenwidth()) / 2
         y = (self.master.winfo_screenheight()) / 3
-        self.master.geometry("+%d+%d" % (x,y))
+        self.master.geometry("+%d+%d" % (x, y))
 
         self.master.config(menu=tk.Menu(self.master))
 
@@ -26,6 +28,7 @@ class App(tk.Frame):
                                                                                                  columnspan=2,
                                                                                                  sticky='w')
 
+        # formatting for all of the inputs and buttons in the interface
         tk.Label(text_frame, text='Normal Stress in X: ').grid(row=2, column=0, sticky='w')
         self.user_sigA = tk.Entry(text_frame, background='white', width=24)
         self.user_sigA.grid(row=2, column=1, sticky='w')
@@ -53,11 +56,17 @@ class App(tk.Frame):
         self.user_units.grid(row=10, column=1, sticky='w')
         self.user_units.focus_set()
 
-        tk.Button(self, text='Create Mohrs circle', command=self.create_mohrs_circle).grid(row=12, column=1)
+        tk.Label(text_frame, text='Show Principal Plane: ').grid(row=12, column=0, sticky='w')
+        self.checkvar = tk.IntVar()
+        self.checkvar.set(0)
+        self.user_principal = tk.Checkbutton(text_frame, variable=self.checkvar, onvalue=1, offvalue=0, width=18)
+        self.user_principal.grid(row=12, column=1, sticky='w')
+        self.user_principal.focus_set()
+
+        tk.Button(self, text='Create Mohrs circle', command=self.create_mohrs_circle).grid(row=14, column=1)
 
         pic_frame = tk.Frame(self)
-        pic_frame.grid(row=14, column=0, rowspan=10, columnspan=10)
-
+        pic_frame.grid(row=16, column=0, rowspan=10, columnspan=10)
 
     # def show_image(self):
     #     load = Image.open('')
@@ -69,18 +78,28 @@ class App(tk.Frame):
 
 
     def create_mohrs_circle(self):
+        # toggle button for showing the principal plane
+        if self.checkvar.get() == 0:
+            show_prin = False
+        if self.checkvar.get() == 1:
+            show_prin = True
+
+        # creates the mohrs circle
         circle = Mc.MohrsFindPrincipal(
                                          float(self.user_sigA.get()),
                                          float(self.user_sigB.get()),
                                          float(self.user_tau.get()),
                                          tmpdir,
                                          float(self.user_angle.get()),
-                                         self.user_units.get()
+                                         self.user_units.get(),
+                                         show_principal=show_prin
                                          )
         circle.visualize()
 
+        # button that allows to save picture in a file of the users choice
         tk.Button(self, text='Save Image As...', command=self.file_saver).grid(row=16, column=0)
 
+        # finds the file saved in the temp directory and uses pillow to load the image then tkinter renders it
         path = os.path.join(tmpdir, 'mohrs.png')
 
         load = Image.open(path)
@@ -91,7 +110,7 @@ class App(tk.Frame):
         img.image = render
 
     def file_saver(self):
-
+        # method to allow user to search directories to save picture of the circle
         path = os.path.join(tmpdir, 'mohrs.png')
         save_image = Image.open(path)
         save = filedialog.asksaveasfilename(parent=self, title='choose file',
@@ -106,6 +125,7 @@ if __name__ == '__main__':
     root = tk.Tk()
     app = App(root)
 
+    # deletes the temp dir and everything in it on close
     def on_closing():
         shutil.rmtree(tmpdir)
         root.destroy()
--- Mohrs_Circle.py
+++ Mohrs_Circle.py
@@ -7,7 +7,6 @@ import os
 # matplotlib.use('Qt5Agg')
 
 SIGFIGS = 3
-UNITS = str('kPa')
 
 
 class MohrsFindPrincipal:
@@ -41,12 +40,12 @@ class MohrsFindPrincipal:
         else:
             alpha = (np.arctan(self.tau / (self.sigma_a - self.center_x)) * 180) / np.pi
 
+        # this finds angles and puts them all in a list of angles
         theta_p1 = alpha / 2
         theta_p2 = theta_p1 + 90
         theta_pure_shear_1 = (-90 + alpha) / 2
         theta_pure_shear_2 = (theta_pure_shear_1 + 90)
         self.angles = [alpha, theta_p1, theta_p2, theta_pure_shear_1, theta_pure_shear_2]
-        print(alpha)
         if self.angle != 0:
             beta = (((-2 * self.angle) + alpha) * np.pi) / 180
             max_inplane_x = self.center_x + (self.rad * np.cos(beta))
@@ -59,23 +58,31 @@ class MohrsFindPrincipal:
             return [self.center_x, self.rad, p1, p2, theta_p1, theta_p2, theta_pure_shear_1, theta_pure_shear_2]
 
     def visualize(self):
+        # this function creates a mohrs circle with the parameters passed to the class
+        # it then creates all of the labels and used matplotlib and pillow to save the created graph as a png
+        # this saves to a temp dir
         principal = self.principal_stress()
 
-        fig = plt.figure(figsize=(10, 5))
+        # this configures the correct plot sizing
+        plt.figure(figsize=(10, 5))
 
-        # configurable options for what displays
+        # configurable options for what displays depending on whether an additional plane angle is input
         if self.angle != 0 and self.show_principal is True:
             x_values = [principal[0], self.sigma_a, self.sigma_b, principal[2], principal[3], self.center_x,
-                        self.center_x, principal[8], principal[9]]
-            y_values = [0, self.tau, -self.tau, 0, 0, self.rad, -self.rad, -principal[10], principal[10]]
+                        self.center_x, principal[8], principal[9], principal[8], principal[9]]
+            y_values = [0, self.tau, -self.tau, 0, 0, self.rad, -self.rad, -principal[10],
+                        principal[10], -principal[10], principal[10]]
             points_label = ['Center', 'Stress in x', 'Stress in y', 'Principal stress x', 'Principal stress y',
-                            'Max shear', 'Min shear', 'In-plane max stress x', 'In-plane max stress y']
+                            'Max shear', 'Min shear', 'In-plane max stress x', 'In-plane max stress y',
+                            'In-plane max shear stress', 'In-plane min shear stress']
         elif self.angle != 0 and self.show_principal is False:
             x_values = [principal[0], self.sigma_a, self.sigma_b, self.center_x, self.center_x,
-                        principal[8], principal[9]]
-            y_values = [0, self.tau, -self.tau, self.rad, -self.rad, -principal[10], principal[10]]
+                        principal[8], principal[9], principal[8], principal[9]]
+            y_values = [0, self.tau, -self.tau, self.rad, -self.rad, -principal[10],
+                        principal[10], -principal[10], principal[10]]
             points_label = ['Center', 'Stress in x', 'Stress in y', 'Max shear', 'Min shear', 'In-plane max stress x',
-                            'In-plane max stress y']
+                            'In-plane max stress y', 'In-plane max shear stress',
+                            'In-plane min shear stress']
         elif self.angle == 0 and self.show_principal is False:
             x_values = [principal[0], self.sigma_a, self.sigma_b, self.center_x, self.center_x]
             y_values = [0, self.tau, -self.tau, self.rad, -self.rad]
@@ -87,12 +94,55 @@ class MohrsFindPrincipal:
             points_label = ['Center', 'Stress in x', 'Stress in y', 'Principal stress x', 'Principal stress 2y',
                             'Max shear', 'Min shear']
 
-        # labels the points for the legend
-        for i in range(len(points_label)):
-            points_label[i] = (str(points_label[i]) + '= (' + str(round_sigfigs((x_values[i]), SIGFIGS)) + ', ' +
-                               str(round_sigfigs((y_values[i]), SIGFIGS)) + ') ' + self.units)
+        # labels the points for the legend and gives each a value
+        points_label[0] = (str(points_label[0]) + '= ' + str(round_sigfigs((x_values[0]), SIGFIGS)) +
+                           str(round_sigfigs((y_values[0]), SIGFIGS)) + self.units)
+        if self.show_principal is True:
+            for i in range(1, 5):
+                points_label[i] = (str(points_label[i]) + '= '
+                                   + str(round_sigfigs((x_values[i]), SIGFIGS))
+                                   + self.units)
 
-        colors = ['black', 'darkred', 'red', 'moccasin', 'orange', 'lightgreen', 'darkgreen', 'purple', 'mediumorchid']
+            for i in range(5, 7):
+                points_label[i] = (str(points_label[i]) + '= '
+                                   + str(round_sigfigs((y_values[i]), SIGFIGS))
+                                   + self.units)
+
+            if self.angle != 0:
+                for i in range(7, 9):
+                    points_label[i] = (str(points_label[i]) + '= ' +
+                                       str(round_sigfigs((x_values[i]), SIGFIGS))
+                                       + self.units)
+                for i in range(9, 11):
+                    points_label[i] = (str(points_label[i]) + '= ' +
+                                       str(round_sigfigs((y_values[i]), SIGFIGS))
+                                       + self.units)
+
+        if self.show_principal is False:
+            for i in range(1, 3):
+                points_label[i] = (str(points_label[i]) + '= '
+                                   + str(round_sigfigs((x_values[i]), SIGFIGS))
+                                   + self.units)
+
+            for i in range(3, 5):
+                points_label[i] = (str(points_label[i]) + '= '
+                                   + str(round_sigfigs((y_values[i]), SIGFIGS))
+                                   + self.units)
+
+            if self.angle != 0:
+                for i in range(5, 7):
+                    points_label[i] = (str(points_label[i]) + '= ' +
+                                       str(round_sigfigs((x_values[i]), SIGFIGS))
+                                       + self.units)
+                for i in range(7, 9):
+                    points_label[i] = (str(points_label[i]) + '= ' +
+                                       str(round_sigfigs((y_values[i]), SIGFIGS))
+                                       + self.units)
+
+
+
+        colors = ['black', 'darkred', 'red', 'moccasin', 'orange', 'lightgreen',
+                  'darkgreen', 'purple', 'mediumorchid', 'lightseagreen', 'gold']
         # prints points
         for i in range(len(x_values)):
             plt.scatter(x_values[i], y_values[i], marker='o', color=colors[i], label=points_label[i])
@@ -105,14 +155,14 @@ class MohrsFindPrincipal:
         t = np.linspace(0, math.radians(self.angles[0]), n + 1)
         x = (self.rad / 3) * np.cos(t) + self.center_x
         y = (self.rad / 3) * np.sin(t)
-        plt.plot(x, y, label='alpha = ' + str(round(-(self.angles[0]))), color='blue')
+        plt.plot(x, y, label='alpha = ' + str(round(self.angles[0])) + ' degrees', color='blue')
 
         # plot phi
         n = 64
         t = np.linspace(math.radians(self.angles[0]), math.radians(90), n + 1)
         x = (self.rad / 2) * np.cos(t) + self.center_x
         y = (self.rad / 2) * np.sin(t)
-        plt.plot(x, y, label='phi = ' + str(round(-(90 - self.angles[0]))), color='orange')
+        plt.plot(x, y, label='phi = ' + str(round(90 - self.angles[0])) + ' degrees', color='orange')
 
         # plot beta
         if self.angle != 0:
@@ -120,7 +170,7 @@ class MohrsFindPrincipal:
             t = np.linspace(math.radians(self.angles[0]), math.radians(self.angles[5]), n + 1)
             x = (self.rad / 2.5) * np.cos(t) + self.center_x
             y = (self.rad / 2.5) * np.sin(t)
-            plt.plot(x, y, label='beta = ' + str(round(-(self.angles[5] - self.angles[0]))), color='green')
+            plt.plot(x, y, label='beta = ' + str(round(self.angles[5] - self.angles[0])) + ' degrees', color='green')
 
         # displays circle
         n = 64
@@ -152,6 +202,7 @@ class MohrsFindPrincipal:
         plt.savefig(path)
 
 
+# this method returns any number of sig figs for a given number
 def round_sigfigs(num, sig_figs):
 
     if num != 0:
@@ -159,7 +210,7 @@ def round_sigfigs(num, sig_figs):
     else:
         return 0
 
+# this code will test the mohrs circle and save a picture of it into the temp dir that it creates
 
-# circle = Mohrs_Find_Principal(STRESS_IN_X, STRESS_IN_Y, SHEAR_XY, ANGLE, SHOW_PRINCIPAL)
-#
+# circle = Mohrs_Find_Principal(STRESS_IN_X, STRESS_IN_Y, SHEAR_XY, ANGLE, UNITS, SHOW_PRINCIPAL)
 # circle.visualize()
